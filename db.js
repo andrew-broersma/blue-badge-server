f986a31faf8218d02f3d3097ca6edfa9a8b38940
Kyle Ketchmark <kyle.r.ketchmark@gmail.com>
2021-12-17 07:29:21 -0800

    db.js updated

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const {Sequelize} = require("sequelize")
 
-const Sequelize = new Sequelize(process.env.DATABASE_URL, {
+const db = new Sequelize(process.env.DATABASE_URL, {
     dialect: 'postgres',
     ssl: process.env.ENVIRONMENT === 'production'
 })
